84110a717eff5251b5ddfc0233fc0ba2c24c8a8c
Raycelle <raycelle@firstcodeacademy.com>
2018-03-02 22:54:59 +0800

    l8

--- Phaser/app.js
+++ Phaser/app.js
@@ -38,8 +38,72 @@ function create(){
 
 	lifelabel = game.add.text(10,5, "Lives: ", style);
 	lifetext = game.add.text(120,5, lives, style);
+
+
+	//lesson 8 
+
+	player = game.add.sprite(32, 400, 'dude')
+		player.animations.add('left',[0,1,2,3],10,true);
+		player.animations.add('right',[5,6,7,8],10,true);
+		game.physics.arcade.enable(player);
+		player.body.gravity.y = 300;
+		player.body.bounce.y = 0.2;
+		player.body.collideWorldBounds = true;
+
+	//create enemy
+	enemy1 = game.add.sprite(760, 20, 'baddie')
+		enemy1.animations.add('left',[0,1],10,true);
+		enemy1.animations.add('right',[2,3],10,true);
+		game.physics.arcade.enable(enemy1);
+		enemy1.body.gravity.y = 500;
+		enemy1.body.bounce.y = 0.2;
+		enemy1.body.collideWorldBounds = true;
+
+	//create stars
+	stars = game.add.physicsGroup();
+	stars.enableBody = true;
+	//use a loop to create 12 stars
+	for(var i = 0; i < 12; i++){
+		var star = stars.create(i * 70,0, 'star');
+		star.body.gravity.y = 200;
+		star.body.bounce.y = 0.7 + Math.random()* 0.2;
+	}
+
+	//create keyboard entries
+	cursors = game.input.keyboard.createCursorKeys();
+
+
 }
 
 function update(){
+	game.physics.arcade.collide(player, platforms);
+	game.physics.arcade.collide(stars, platforms);
+	game.physics.arcade.collide(enemy1, platforms);
+
+	//set player velocity to 0 if no events
+	player.body.velocity.x = 0;
+
+	if(cursors.left.isDown){
+		//move left
+		player.animations.play('left');
+		player.body.velocity.x = -150;
+	} else if(cursors.right.isDown){
+		//move left
+		player.animations.play('right');
+		player.body.velocity.x = 150;
+	} else {
+		player.animations.stop();
+		player.frame = 4;
+	}
+
+	if(cursors.up.isDown && player.body.touching.down){
+		player.body.velocity.y = -300;
+	}
 
 }
+
+
+
+
+
+
